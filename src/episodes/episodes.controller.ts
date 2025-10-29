3283ad4eb41b89d58aef0c8a489d953e58b10d61
Shah Nawaz <shuvo1824@hotmail.com>
2025-10-29 23:51:52 +0600

    chore: service methods added in controller

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,21 +1,29 @@
 import { Controller, Query, Param, Get, Post, Body } from '@nestjs/common';
+import { EpisodesService } from './episodes.service';
 
 @Controller('episodes')
 export class EpisodesController {
+  constructor(private episodesService: EpisodesService) {}
+
   @Get()
   findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
     console.log(sort);
-    return 'This action returns all episodes';
+    return this.episodesService.findAll(sort);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns episode #${id}`;
+    return this.episodesService.findOne(id);
+  }
+
+  @Get('featured')
+  findFeatured() {
+    return this.episodesService.findFeatured();
   }
 
   @Post()
   create(@Body() input: any) {
     console.log(input);
-    return 'This action adds a new episode';
+    return this.episodesService.create(input);
   }
 }
