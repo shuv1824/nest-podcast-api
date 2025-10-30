f931b4d0673f15f7f5441f016791feb35e25f013
Shah Nawaz <shuvo1824@hotmail.com>
2025-10-30 22:50:31 +0600

    chore: config service

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Query, Param, Get, Post, Body } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
+import { ConfigService } from 'src/config/config.service';
 
 @Controller('episodes')
 export class EpisodesController {
-  constructor(private episodesService: EpisodesService) {}
+  constructor(
+    private episodesService: EpisodesService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
